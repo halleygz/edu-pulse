0237ec01d815aa2e4c9da7746089741049d60a77
Halleluya Mulugeta <halleluya2013idone@gmail.com>
2024-09-03 22:34:10 +0300

    added authenticator middleware

---
+++ server/src/middlewares/authToken.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { config } from "../config";
+import User from "../models/User";
+import { IUser } from "../types/modelTypes/UserType";
+
+//Extend the Request interface to include the user property
+interface AuthRequested extends Request {
+  user?: IUser;
+}
+
+// Middleware to check if user is authenticated
+const authenticatToken = async (
+  req: AuthRequested,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, config.jwt.secret) as { id: string };
+    const user = await User.findById(decoded.id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error: any) {
+    return res.status(403).json({ message: "Unauthorized" });
+  }
+};
+
+export { authenticatToken, AuthRequested };
