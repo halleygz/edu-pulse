a1432c30bd37375d543d617d5e8c1a1389e04ee9
Dagmawi <dagmawitewodros111@gmail.com>
2024-09-07 22:01:09 +0300

    added test component

---
+++ components/Home/TestYour/TestYour.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const KnowledgeTest: React.FC = () => {
+  return (
+    <div className="bg-white p-8">
+      <div className="bg-custom-green text-white py-10 px-12 mx-auto mt-6 rounded-md max-w-3xl text-center">
+        <h2 className="text-2xl font-bold mb-4">Test Your Knowledge</h2>
+        <p className="text-lg">
+          Ready to see how far you have come? Dive into our quick test and measure your understanding.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default KnowledgeTest;
