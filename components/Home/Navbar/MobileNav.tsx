d709280a85dcd8bbe5342576221d1789127bed52
halleluya2013idone <halleluya2013idone@gmail.com>
2024-09-07 21:49:16 +0000

    Landing-Page

--- components/Home/Navbar/MobileNav.tsx
+++ components/Home/Navbar/MobileNav.tsx
@@ -26,10 +26,10 @@ const MobileNav = ({ closeNav, showNav }: Props) => {
                     {/* Logo */}
                     <h2 className='text-2xl font-extrabold text-center mt-12 mb-8'>EduPlus</h2>
                     {/* SearchBar and Profile */}
-                    <div className='flex flex-col items-center space-y-4'>
+                    {/* <div className='flex flex-col items-center space-y-4'>
                         <SearchBar />
                         <Profile />
-                    </div>
+                    </div> */}
                     {/* Navlinks */}
                     <div className='flex flex-col items-center mt-6'>
                         {navLinks.map((link) => (
