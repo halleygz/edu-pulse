83f2399e35c642faee348adede7bc717a1bb3773
Dagmawi <dagmawitewodros111@gmail.com>
2024-09-07 21:42:42 +0300

    added the object for the navigation

---
+++ constant/constant.ts
@@ -0,0 +1,22 @@
+export const navLinks = [ 
+    {
+        id: 1,
+        url: "#home",
+        label: 'Home'
+    },
+    {
+        id: 2,
+        url: "#about",
+        label: 'About'
+    },
+    {
+        id: 4,
+        url: "#testimonials",
+        label: 'Testimonials'
+    },
+    {
+            id: 5,
+            url: "",
+            label: "Daily learning"
+    },
+];
