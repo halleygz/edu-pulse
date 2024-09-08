0556199b3bc7a82b940ec23743a03b7773b82889
halleluya2013idone <halleluya2013idone@gmail.com>
2024-09-08 15:11:23 +0000

    updated server controllers

--- server/src/utils/genTokenSetCookie.ts
+++ server/src/utils/genTokenSetCookie.ts
@@ -8,9 +8,9 @@ const genTokenSetCookie = (res: any, user: any) => {
 
   res.cookie('token', token, {
     maxAge: 1000 * 60 * 60 * 24 * 10,
-    // httpOnly: true,
-    // secure: true,
-    sameSite: 'strict',
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
   });
 }
 
