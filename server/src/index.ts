cd3eb9597f3003e2f2b085e0af496ffe4cdfed41
Halleluya Mulugeta <98106633+halleygz@users.noreply.github.com>
2024-09-09 09:53:55 +0300

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,11 +11,11 @@ dotenv.config();
 const app: Express = express();
 const PORT = config.server.port;
 
-app.use(cors({
-  origin: "https://3000-idx-pla-directed-1725603589203.cluster-4ezwrnmkojawstf2k7vqy36oe6.cloudworkstations.dev",
-  credentials: true,
-  allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept"
-}));
+// app.use(cors({
+  // origin: "https://3000-idx-pla-directed-1725603589203.cluster-4ezwrnmkojawstf2k7vqy36oe6.cloudworkstations.dev",
+  //credentials: true,
+  //allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept"
+//}));
 app.use(express.json());
 app.use(cookieParser());
 
