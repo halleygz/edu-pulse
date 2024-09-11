dccdba01a465c708f9b2dea15b6ff0cb69a6a7ea
Dagmawi <dagmawitewodros111@gmail.com>
2024-09-11 20:10:17 +0300

    modified the navigation bar

--- client/components/Home/Hero/Hero.tsx
+++ client/components/Home/Hero/Hero.tsx
@@ -7,7 +7,8 @@ import HeroImage from './HeroImage';
 
 const Hero = () => {
   return (
-    <div className='w-full pt-[4vh] md:pt-[12vh] h-screen bg-custom-green' id='home'>
+    // Removed top padding to eliminate the gap above the hero section
+    <div className='w-full h-screen bg-custom-green' id='home'>
       <div className="flex justify-center flex-col w-4/5 h-full mx-auto">
         <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-12">
           {/* Text Content */}
