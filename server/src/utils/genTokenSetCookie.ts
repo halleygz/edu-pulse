0201f6188182cdbc185dc7afa17f2603eebb2a88
Halleluya Mulugeta <98106633+halleygz@users.noreply.github.com>
2024-09-15 19:36:58 +0300

    Update genTokenSetCookie.ts

--- server/src/utils/genTokenSetCookie.ts
+++ server/src/utils/genTokenSetCookie.ts
@@ -9,7 +9,7 @@ const genTokenSetCookie = (res: any, user: any) => {
   res.cookie('token', token, {
     maxAge: 1000 * 60 * 60 * 24 * 10,
     httpOnly: true,
-    /* secure: true, */
+    secure: true,
     sameSite: 'none',
   });
 }
